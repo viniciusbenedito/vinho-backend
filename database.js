934bca128ab7d38841b5d419326e7d90a47bdfa5
Vinícius Benedito <vinicius.benedito98@gmail.com>
2020-06-09 21:01:00 +0000

    Versão 2

---
+++ database.js
@@ -0,0 +1,34 @@
+// yarn add pg
+
+const Pool= require('pg').Poll;
+
+// 1 - Abrir a conexão
+// 2 - Executar o comando SQL (query, insert) 30ms (índice)
+// 3 - Fechar a conexão
+
+const pool = new Pool({
+    user:'eiaxgsukppdhtn',
+    password:'f2a4ad85d074123efabac172742aa12270422eb164b5ea90e5aad1dc18808d9a',
+    host:'ec2-3-222-30-53.compute-1.amazonaws.com',
+    database:'d73usfjf2rnoh5',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+const sql = `
+    CREATE TABLE IF NOT EXISTS vinho
+    (
+        ID serial primary key,
+        nome varchar(50) not null,
+        tipo varchar(30) not null,
+        classificacao varchar(30) not null,
+        safra int not null
+    )
+`;
+
+pool.query(sql, function(error, result) {
+    if(error)
+        throw error
+
+    console.log('Tabela criada com sucesso!');
+})
